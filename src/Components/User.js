4371dd54ea3929d9efbb1564cd5d2598cb3b0527
WebDev2105 <choudareddy9842@gmail.com>
2024-10-04 21:31:16 +0530

    Added Class Based Components

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 const User = () => {
+  const [count] = useState(0);
+  const [count2] = useState(2);
   return (
     <div className="user-card">
+      <h1>Count:{count}</h1>
+      <h1>Count2:{count2}</h1>
       <h1>Name:Chinna</h1>
       <h2>Location:Hyderabad</h2>
     </div>
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -2,11 +2,26 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
+        <h1>Count: {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Click Me
+        </button>
+
         <h1>Name: {name}</h1>
         <h2>Location: {location}</h2>
       </div>
